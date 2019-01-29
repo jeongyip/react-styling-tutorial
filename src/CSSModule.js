377feda4a917e21f2f76ecbca5056af5d99bb1c4
jeongyip <jeongyip@gmail.com>
2019-01-29 23:45:46 +0900

    style 수정

---
+++ src/CSSModule.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './CSSModule.module.css';
+import classNames from 'classnames';
+
+const cx = classNames.bind(styles);
+
+const CSSModule = () => {
+  console.log(styles);
+
+  return (
+    <div className={cx('wrapper', 'inverted')}>
+      안녕하세요, 저는 <span className="something">CSS Module!</span>
+    </div>
+  );
+};
+
+export default CSSModule;
